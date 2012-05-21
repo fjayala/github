3fad009047065e89a46c69166ae7c54897bcd0b6
fjayala <fjayala@106-17.info2>
2012-05-21 08:49:25 +0200

    aaa

--- github/src/esoes.java
+++ github/src/esoes.java
@@ -5,8 +5,7 @@ public class esoes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("esto es una prueba de master");
+
 	}
 
 }
